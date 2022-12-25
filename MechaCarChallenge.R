b2ae6eed2d21655ccb2190e38a7d9470651715fb
Rachel Helmich <101941048+midwestbotanist@users.noreply.github.com>
2022-12-24 20:40:44 -0600

    Adding linear regression and lot/total summaries

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,7 +16,22 @@ mpg_lm = lm(mpg ~ ., data=mechaCar_mpg_df)
 # Find p-value and r-squared value using the summary() function
 summary(mpg_lm)
 
+
 # PART 2: Summary Statistics on Suspension Coils
+# Import and read Suspension_Coil.csv as a dataframe and check data
+suspension_Coil_df <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+head(suspension_Coil_df)
+
+# Create a total_summary dataframe using the summarize() function
+# Find the mean, median, variance, and standard deviation of the PSI column, view total_summary
+# Load dyplr and magrittr library to use %>%: https://stackoverflow.com/questions/30248583/error-could-not-find-function
+total_summary <- suspension_Coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+total_summary
+
+# Create a lot_summary dataframe using group_by() and summarize()
+# Group lots by mean, median, vairance, and standard deviation of the PSI column, view lot_summary
+lot_summary <- suspension_Coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') 
+lot_summary
 
 # PART 3: T-Test on Suspension Coils
 
